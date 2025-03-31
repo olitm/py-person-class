8fbb20e0a5e13d8b5b5591a576df70bf09d0f0ac
Ol <holmate@dontsp.am>
2025-03-31 11:13:20 +0300

    Changes made according to the recommendations

--- app/main.py
+++ app/main.py
@@ -8,21 +8,18 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    persons = []
-
-    for person_data in people:
-        name = person_data["name"]
-        age = person_data["age"]
-        person = Person(name, age)
-        persons.append(person)
+    persons = [Person(data["name"], data["age"]) for data in people]
 
     for person_data in people:
         name = person_data["name"]
         person = Person.people[name]
 
-        if "wife" in person_data and person_data["wife"]:
-            person.wife = Person.people[person_data["wife"]]
-        if "husband" in person_data and person_data["husband"]:
-            person.husband = Person.people[person_data["husband"]]
+        wife_name = person_data.get("wife")
+        if wife_name:
+            person.wife = Person.people.get(wife_name)
+
+        husband_name = person_data.get("husband")
+        if husband_name:
+            person.husband = Person.people.get(husband_name)
 
     return persons
